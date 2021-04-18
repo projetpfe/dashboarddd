9e6046ee0977e04830980c8037b1dcc4956e8eef
etidel <abdelkader.iitidel@gmail.com>
2021-04-18 12:44:33 +0100

    first commit

---
+++ adminproject/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def homepage(request):
+     return render(request, 'index.html')
+
+
+
+
+
+
